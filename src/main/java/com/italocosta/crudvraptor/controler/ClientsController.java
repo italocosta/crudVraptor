9ed83f54d67ac92da4d6a8b389148243c429c822
Italo Costa <italo.costa.ce@gmail.com>
2017-09-30 00:26:20 -0300

    Commit initial with config vraptor

---
+++ src/main/java/com/italocosta/crudvraptor/controler/ClientsController.java
@@ -0,0 +1,13 @@
+package com.italocosta.crudvraptor.controler;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Path;
+
+@Controller
+public class ClientsController {
+	
+	@Path("/")
+	public void listClients() {
+		
+	}
+}
